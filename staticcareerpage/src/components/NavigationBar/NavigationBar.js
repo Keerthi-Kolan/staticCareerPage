c36a34df06d67c2429213d432a2ada66b7840ba8
Keerthi-Kolan <kolankeerthi.reddy@gmail.com>
2025-02-21 01:33:48 -0800

    navbar added

---
+++ staticcareerpage/src/components/NavigationBar/NavigationBar.js
@@ -0,0 +1,19 @@
+const NavigationBar = () => {
+  return (
+    <div className="navbar">
+      <div>
+        <h1>ShareCal</h1>
+      </div>
+      <div className="links">
+        <a link="#">Customer Story</a>
+        <a link="#">Blog</a>
+        <a link="#">Careers</a>
+        <a link="#">Security</a>
+        <a link="#">Pricing</a>
+        <a link="#">login</a>
+        <button>Sign up</button>
+      </div>
+    </div>
+  );
+};
+export default NavigationBar;
